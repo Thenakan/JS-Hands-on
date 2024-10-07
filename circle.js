972680e9bde26e853144864bd63ce5679c0d77d2
Thenakan <thenakan12@gmail.com>
2024-10-07 18:52:31 +0530

    answer added

---
+++ circle.js
@@ -0,0 +1,5 @@
+function area(radius){
+    return (22/7)*radius*radius
+}
+let circleArea=area(7)
+console.log(circleArea)
---
+++ prime.js
@@ -0,0 +1,12 @@
+function isPrime(num) {
+    if (num <= 1) return false; 
+    for (let i = 2; i < num; i++) {
+        if (num % i === 0) {
+            return false; 
+        }
+    }
+    return true; 
+}
+const userInput = 11;
+console.log(`Is ${userInput} a prime number? `, 
+    isPrime(userInput));
